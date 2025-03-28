8cf29e00f61413941b678ec5bb94b7f9f539e29c
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-03-28 13:20:19 +0000

    create and debug the while loop in 'playersChoice'

--- run.py
+++ run.py
@@ -1,36 +1,39 @@
 from tabulate import tabulate 
 
 gameboard = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
 ]
-print(tabulate(gameboard, tablefmt="rounded_grid"))
+print(tabulate(gameboard, tablefmt="rounded_grid")) # add tabulate headers.
 
-#def PlayersChoice():
-   # print("Would you like to be X or O?")
-   # if "x":
-def checkEmpty(search_item):
-    """
-    Checks if the location on the board where the player wants to place their X is free
-    If it's free, it allows the player to place it there and passes it to the function to see if they've got three in a row"""
-    try:
-        value = # this value needs to be equal to the location on the gameboard where the player is requesting to place their item.
-        if value == search_item:
-            #replace the value in the board with the search item (X)
-        else:
-            return False
-    except IndexError:
-        print(f"Invalid row or column index. Ensure you're using a valid index.")
-        return False1
     
-
+def playersChoice():
+    while True:
+        player = input("Player, please choose, X or O for your go:\n").strip().upper()
+        if player == "X" or player == "O":
+            print(f"you have chosen {player.upper()}")
+            break
+        else:
+                player = input("Player, please choose, X or O for your go:\n").strip().upper()
 
 def PlayerMove():
+    """
+    where the player decides where to put their x / o
+    """
+    player = playersChoice()
     while True:
         try:
-            search_item = input("Choose your position based on the numbers:\n")
-            checkEmpty(search_item)
+            row_index = int(input("Choose you row between 0 & 2:"))
+            column_index = int(input("Choose your column between 0 & 2:"))
+            cell_value = gameboard[row_index][column_index]
+            
+            if cell_value == "":
+                cell_value = player
+                break
+            else:
+                print("Cell already taken, try again.")
+                return False
             
         except (ValueError, IndexError):
             print("Invalid input, please try again")
@@ -39,5 +42,4 @@ def PlayerMove():
 
 def main():
     PlayerMove()
-    checkEmpty()
 main()
