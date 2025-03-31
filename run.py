f2e58d6f118376930b527973a0f021f0a16811c3
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-03-31 15:19:31 +0100

    pep 8 check and fix

--- run.py
+++ run.py
@@ -2,16 +2,13 @@ from tabulate import tabulate
 import random
 import sys
 
+
 class GameBoard:
     """
     Tic Tac Toe board
     """
     def __init__(self):
-        self.board = [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""]
-    ]
+        self.board = [["", "", ""], ["", "", ""], ["", "", ""]]
 
     def printGameboard(self):
         headers = ["Column 0", "Column 1", "Column 2"]
@@ -26,29 +23,29 @@ class GameBoard:
             self.board[row][col] = symbol
             return True
         return False
-    
+
     def check_Won(self, symbol):
-            """
-            looks at the row to see if the player / computer has 3 in a row
-            """
-            for row in range(3):
-                if self.board[row][0] == self.board[row][1] == self.board[row][2] == symbol:
-                    return True
-            for col in range(3):
-                if self.board[0][col] == self.board[1][col] == self.board[2][col] == symbol:
-                    return True
-            if self.board[0][0] == self.board[1][1] == self.board[2][2] == symbol:
-                """
-                    Checks diagonal (top left to bot. right)
-                    """
+        """
+        looks at the row to see if the player / computer has 3 in a row
+        """
+        for row in range(3):
+            if self.board[row][0] == self.board[row][1] == self.board[row][2] == symbol:
                 return True
-            if self.board[0][2] == self.board[1][1] == self.board[2][0] == symbol:
-                """
-                Checks diagonal (top right to bot. left)
-                """
+        for col in range(3):
+            if self.board[0][col] == self.board[1][col] == self.board[2][col] == symbol:
                 return True
-            
-            return False
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] == symbol:
+            """
+            Checks diagonal (top left to bot. right)
+            """
+            return True
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] == symbol:
+            """
+            Checks diagonal (top right to bot. left)
+            """
+            return True
+
+        return False
 
     def checkDraw(self):
         for row in self.board:
@@ -56,6 +53,7 @@ class GameBoard:
                 return False
         print("You drew!")
         return True
+
 
 class Player:
     """
@@ -70,13 +68,14 @@ class Player:
         """
         return self.symbol
 
+
 class TicTacToeGame:
     def __init__(self):
         self.board = GameBoard()
         self.player = None
         self.computer = None
         self.curren_player = None
-    
+
     def playersChoice(self):
         """
         sets up player as "x" or "o"
@@ -91,40 +90,40 @@ class TicTacToeGame:
         self.computer = Player("O" if player_symbol == "X" else "X")
 
     def playerMove(self):
-            """
-            where the player decides where to put their x / o
-            """
-            self.board.printGameboard()
+        """
+        where the player decides where to put their x / o
+        """
+        self.board.printGameboard()
 
-            while True:
-                try:
-                    row_index = int(input("Choose your row between 0 & 2:"))
-                    column_index = int(input("Choose your column between 0 & 2:"))
+        while True:
+            try:
+                row_index = int(input("Choose your row between 0 & 2:"))
+                column_index = int(input("Choose your column between 0 & 2:"))
 
-                    if self.board.update_cell(row_index, column_index, self.player.get_symbol()):
-                        break
-                    else:
-                        print("Cell already taken, try again.")
-                except (ValueError, IndexError):
-                    print("Invalid input, please try again")
+                if self.board.update_cell(row_index, column_index, self.player.get_symbol()):
+                    break
+                else:
+                    print("Cell already taken, try again.")
+            except (ValueError, IndexError):
+                print("Invalid input, please try again")
 
     def computerMove(self):
-            """
-            The computer's Turn
-            """
-            self.board.printGameboard()
-            while True:
-                try:
-                    row_index = random.randint(0, 2)
-                    column_index = random.randint(0, 2)
+        """
+        The computer's Turn
+        """
+        self.board.printGameboard()
+        while True:
+            try:
+                row_index = random.randint(0, 2)
+                column_index = random.randint(0, 2)
 
-                    if self.board.update_cell(row_index, column_index, self.computer.get_symbol()):
-                        print("Computer's Turn...")
-                        print(f"Computer plays: row: {row_index} & column: {column_index}")
-                        break
-                except (ValueError, IndexError):
-                    pass                
-    
+                if self.board.update_cell(row_index, column_index, self.computer.get_symbol()):
+                    print("Computer's Turn...")
+                    print(f"Computer plays: row: {row_index} & column: {column_index}")
+                    break
+            except (ValueError, IndexError):
+                pass
+
     def endGame(self):
         """
         ends the game
@@ -154,8 +153,6 @@ class TicTacToeGame:
             self.endGame()
             return True
         return False
-    
-    
 
     def game(self):
         """
@@ -172,8 +169,8 @@ class TicTacToeGame:
                 self.board.printGameboard()
                 self.endGame()
                 break
-            
-            #computer
+
+            # computer
             self.computerMove()
             if self.checkWinner():
                 break
@@ -182,6 +179,7 @@ class TicTacToeGame:
                 self.board.printGameboard()
                 self.endGame()
                 break
+
 
 def main():
     """
